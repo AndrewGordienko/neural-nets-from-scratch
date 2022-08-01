b71c6cf4e5c3bb0036c2e37b26400735391eb07c
Andrew Gordienko <60705784+AndrewGordienko@users.noreply.github.com>
2022-08-01 02:44:02 -0400

    Update and rename node-network.py to mnist-node-network.py

--- mnist-node-network.py
+++ mnist-node-network.py
@@ -1,6 +1,12 @@
 import random
 from copy import deepcopy
 import numpy as np
+from keras.datasets import mnist
+
+batch_size = 10
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+images, labels = (x_train[0:batch_size].reshape(batch_size, 28*28)/255, y_train[0:batch_size])
+x_test, y_test = (x_test[0:batch_size].reshape(batch_size, 28*28)/255, y_test[0:batch_size])
 
 random.seed(0)
 
@@ -15,17 +21,17 @@ class Connection():
     def __init__(self):
         self.output_node_number = None
         self.output_node_layer = None
-        self.weight = random.uniform(-1, 1)
+        self.weight = random.uniform(-0.1, 0.1)
 
 class Network():
     def __init__(self):
-        self.input_size = 3
-        self.hidden_size = 4
-        self.output_size = 1
+        self.input_size = 784
+        self.hidden_size = 40
+        self.output_size = 10
         self.network_topology = [[], [], []]
         self.amount = [self.input_size, self.hidden_size, self.output_size]
         neuron_total = 0
-        self.lr = 0.01
+        self.lr = 0.005
 
         for i in range(len(self.network_topology)):
             for j in range(self.amount[i]):
@@ -48,7 +54,7 @@ class Network():
         for i in range(len(self.network_topology)):
             for j in range(len(self.network_topology[i])):
                 self.network_topology[i][j].value = 0
-        
+
         for i in range(len(self.network_topology[0])):
             self.network_topology[0][i].value = state[0][i]
         
@@ -70,7 +76,7 @@ class Network():
         last_values = []
         maximum_index = len(self.network_topology)-1
         for i in range(len(self.network_topology[maximum_index])):
-            last_values.append(self.network_topology[maximum_index][i].value)
+            last_values.append(np.tanh(self.network_topology[maximum_index][i].value))
 
         return last_values
     
@@ -86,7 +92,13 @@ class Network():
             for j in range(len(self.network_topology[second_last_layer][i].connections_out)):
                 weight = self.network_topology[second_last_layer][i].connections_out[j].weight
                 deriv = self.relu2deriv(self.network_topology[second_last_layer][i].value)
-                layer_1_delta.append(layer_2_delta[0] * weight * deriv)
+
+                node_number = self.network_topology[second_last_layer][i].connections_out[j].output_node_number
+                for p in range(len(self.network_topology[last_layer])):
+                        if self.network_topology[last_layer][p].number == node_number:
+                            index = p
+
+                layer_1_delta.append(layer_2_delta[0][index] * weight * deriv)
 
         for i in range(len(self.network_topology[second_last_layer])):
             for j in range(len(self.network_topology[second_last_layer][i].connections_out)):
@@ -94,8 +106,8 @@ class Network():
                 for p in range(len(self.network_topology[last_layer])):
                         if self.network_topology[last_layer][p].number == node_number:
                             index = p
-                self.network_topology[second_last_layer][i].connections_out[j].weight -= self.lr * self.network_topology[second_last_layer][i].value * layer_2_delta[index]
-
+                self.network_topology[second_last_layer][i].connections_out[j].weight -= self.lr * self.network_topology[second_last_layer][i].value * layer_2_delta[0][index]
+        
         for i in range(len(self.network_topology[first_layer])):
             for j in range(len(self.network_topology[first_layer][i].connections_out)):
                 node_number = self.network_topology[first_layer][i].connections_out[j].output_node_number
@@ -118,29 +130,22 @@ class Network():
                 print("node {} layer {} number {}".format(self.network_topology[i][j], self.network_topology[i][j].layer_number, self.network_topology[i][j].number))
                 for k in range(len(self.network_topology[i][j].connections_out)):
                     print("connected to node on layer {} number {} weight {}".format(self.network_topology[i][j].connections_out[k].output_node_layer, self.network_topology[i][j].connections_out[k].output_node_number, self.network_topology[i][j].connections_out[k].weight))
-        
-streetlights = np.array([[1, 0, 1],
-                         [0, 1, 1],
-                         [0, 0, 1],
-                         [1, 1, 1],
-                         [0, 1, 1],
-                         [1, 0, 1]])
-walk_vs_stop = np.array([[0, 1, 0, 1, 1, 0]])
 
 network = Network()
 
-print("original network:")
-network.printing_stats()
+#print("original network:")
+#network.printing_stats()
 
 epochs = 40
 for epoch in range(epochs):
     error = 0
 
-    for i in range(len(walk_vs_stop)):
-        input = np.array([streetlights[i]])
-        ground_truth = walk_vs_stop[0][i]
+    for i in range(len(images)):
+        image = images[i:i+1]
+        ground_truth = np.zeros((1, 10))
+        ground_truth[0][labels[i]] = 1
 
-        value = network.forward(input)
+        value = network.forward(image)
 
         error += np.sum((ground_truth - value)**2)
 
@@ -148,5 +153,5 @@ for epoch in range(epochs):
 
     print(f"error is {error}")
 
-print("v2")
-network.printing_stats()
+#print("v2")
+#network.printing_stats()
